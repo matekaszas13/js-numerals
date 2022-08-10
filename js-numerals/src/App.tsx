667d62b32c19abedb23067a56d11bb56967e275c
matekaszas13 <matekaszas17@gmail.com>
2022-08-10 15:30:15 +0200

    Feat: Add useState to track the input value

--- js-numerals/src/App.tsx
+++ js-numerals/src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [inputValue, setInputValue] = useState<number>();
+
   return (
     <div >
       <input type="text" />
